2cb0b87d0e87f5dd3df57995dffdee12ee19172a
hairama <67448768+hairama@users.noreply.github.com>
2024-10-03 12:39:10 -0700

    fixed adding blank items to the cart

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const shoppingListEl = document.getElementById("shopping-list")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    push(shoppingListInDB, inputValue)
+    // if the input field has a value 
+    if( inputValue != "" ) {
+        push(shoppingListInDB, inputValue)
+    }
+    
     
     clearInputFieldEl()
 })
